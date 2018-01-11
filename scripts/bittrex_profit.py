8ed267301a1c3008d1c66cc53855f8c06d49eced
Anton Ermak <anton@ermak.us>
2018-01-11 16:29:05 +0700

    Script to calc profit from bittrex csv

---
+++ scripts/bittrex_profit.py
@@ -0,0 +1,44 @@
+import sys
+from io import BytesIO
+from pandas import DataFrame, read_csv
+from datetime import timedelta
+
+def orders_total_sell(df):
+    return (df['Price']  - df['CommissionPaid']).sum()
+
+def orders_total_buy(df):
+    return (df['Price']  + df['CommissionPaid']).sum()
+
+def print_stat(df, idx):
+    buy  = df.loc[df['Type'] == 'LIMIT_BUY']
+    sell = df.loc[df['Type'] == 'LIMIT_SELL']
+    profit = orders_total_sell(sell) - orders_total_buy(buy)
+    print("{0} {1: .8f} {2: 4d} {3: 4d} {4: 4d}".format( idx, profit, df.shape[0], buy.shape[0], sell.shape[0]))
+
+def full_stats(filename, days):
+    fh = open(filename, 'rb')
+    buf = BytesIO(fh.read().decode('UTF-16').encode('UTF-8'))
+    df = read_csv( buf,
+                   parse_dates=["Opened","Closed"],
+                   index_col="Closed")
+
+    if days:
+        last = df.index[-1].to_pydatetime().replace(hour=0, minute=0, second=0) - timedelta(days=days-1)
+        df = df.loc[df.index >= last]
+        print("Last %s days" % days)
+
+    print("--------------------------------------")
+    print("Date      Profit       Trades Buy Sell")
+    print("--------------------------------------")
+    for idx, day in df.groupby(df.index.date):
+        idx = str(day.index[0])[:10]
+        print_stat(day, idx)
+    print("--------------------------------------")
+    print_stat(df,'Total     ')
+
+try:
+    days = int(sys.argv[2])
+except:
+    days = 0
+
+full_stats(sys.argv[1], days)
